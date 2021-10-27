8e0514eca648e013ab7eb09de6c084618ae44bbc
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-10-27 15:42:47 +0700

    Hugo Project

---
+++ HugoMusic/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from HugoMusic.services.queues.queues import clear 
+from HugoMusic.services.queues.queues import get
+from HugoMusic.services.queues.queues import is_empty
+from HugoMusic.services.queues.queues import put
+from HugoMusic.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
